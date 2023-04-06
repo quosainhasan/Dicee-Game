36c218a6ada024ff7df9c6fe60d98c724105791b
Quosain Hasan <33932120+quosainhasan@users.noreply.github.com>
2023-04-06 20:37:33 +0000

    Change JavaScript

--- script.js
+++ script.js
@@ -1,59 +1,16 @@
-var p1 = prompt("Player 1 name: ");
-var p2 = prompt("Player 2 name: ");
-
-document.querySelector(".p1").textContent = p1;
-document.querySelector(".p2").textContent = p2;
-
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomDiceImage);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 console.log("Player 1: " + randomNumber1);
 console.log("Player 2: " + randomNumber2);
 
-function image1(number1){
-    if(number1 === 1){
-        document.querySelector(".img1").setAttribute("src", "images/dice1.png");
-    }
-
-    if(number1 === 2){
-        document.querySelector(".img1").setAttribute("src", "images/dice2.png");
-    };
-
-    if (number1 === 3){
-        document.querySelector(".img1").setAttribute("src", "images/dice3.png");
-    };
-
-    if(number1 === 4){
-        document.querySelector(".img1").setAttribute("src", "images/dice4.png");
-    };
-
-    if(number1 === 5){
-        document.querySelector(".img1").setAttribute("src", "images/dice5.png");
-    };
-};
-
-function image2(number2){
-    if(number2 === 1){
-        document.querySelector(".img2").setAttribute("src", "images/dice1.png");
-    }
-
-    if(number2 === 2){
-        document.querySelector(".img2").setAttribute("src", "images/dice2.png");
-    };
-
-    if (number2 === 3){
-        document.querySelector(".img2").setAttribute("src", "images/dice3.png");
-    };
-
-    if(number2 === 4){
-        document.querySelector(".img2").setAttribute("src", "images/dice4.png");
-    };
-
-    if(number2 === 5){
-        document.querySelector(".img2").setAttribute("src", "images/dice5.png");
-    };
-};
 
 function dicee(player1, player2){
     if(player1 === player2){
@@ -69,7 +26,3 @@ function dicee(player1, player2){
 };
 
 dicee(randomNumber1, randomNumber2);
-
-image1(randomNumber1);
-
-image2(randomNumber2);
